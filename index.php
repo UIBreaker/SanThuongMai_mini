a24f1ef30b09e518f5b5aae4eb744bd2b1694997
Nhatnamgmailcom <nhatnam171217@gmail.com>
2025-04-02 21:13:12 +0700

    Thêm sửa xóa

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'app/models/ProductModel.php'; 
 
 $url = $_GET['url'] ?? '';
 $url = rtrim($url, '/');
@@ -7,11 +8,12 @@ $url = filter_var($url, FILTER_SANITIZE_URL);
 $url = explode('/', $url);
 
 // Kiểm tra phần đầu tiên của URL để xác định controller
-// URL: /product/list/5 → $controllerName = 'ProductController'
-$controllerName = isset($url[0]) && $url[0] != '' ? ucfirst($url[0]) . 'Controllers' : 'ProductController';
+$controllerName = isset($url[0]) && $url[0] != '' ? ucfirst($url[0]) . 'Controller' : 'ProductController';
 
 // Kiểm tra phần thứ hai của URL để xác định action
 $action = isset($url[1]) && $url[1] != '' ? $url[1] : 'index';
+
+// die ("controller=$controllerName - action=$action");
 
 // Kiểm tra xem controller và action có tồn tại không
 if (!file_exists('app/controllers/' . $controllerName . '.php')) {
